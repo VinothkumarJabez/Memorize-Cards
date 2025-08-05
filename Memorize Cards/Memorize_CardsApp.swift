e6556c74b8720ba8d1d9b78982495dd7498e04e9
delvinajenoline <delvinajenoline@gmail.com>
2025-08-05 10:40:33 +0530

    Initial Commit

---
+++ Memorize Cards/Memorize_CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize_CardsApp.swift
+//  Memorize Cards
+//
+//  Created by Delvina Jenoline on 05/08/25.
+//
+
+import SwiftUI
+
+@main
+struct Memorize_CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
